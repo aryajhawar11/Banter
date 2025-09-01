eb66b1017d73fc15a58ab869ea7c3b10fcd2cd3c
Arya Jhawar <aryajhawar.2004@gmail.com>
2025-09-01 22:02:45 +0530

    auth provider and sentry routes done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,25 +4,31 @@ import HomePage from './pages/HomePage'
 import AuthPage from './pages/AuthPage'
 import { Navigate } from 'react-router'
 import { Routes ,Route } from 'react-router'
+import toast from 'react-hot-toast'
+import * as Sentry from "@sentry/react";
+
+
+const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
 const App = () => {
+  
+
   return (
     <>
-   
+
     <SignedIn>
-      <Routes>
+    <SentryRoutes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<Navigate to={"/"} replace />} />
-      </Routes>
+        <Route path="/auth" element={<Navigate to={"/"} replace />} />  
+      </SentryRoutes>
     </SignedIn>
 
 
     <SignedOut>
-
-      <Routes>
+    <SentryRoutes>
         <Route path="/auth" element={<AuthPage />} />
         <Route path="*" element={<Navigate to={"/auth"} replace />} />
-      </Routes>
+      </SentryRoutes>
     </SignedOut>
   </>
   )
---
+++ frontend/src/lib/axios.js
@@ -0,0 +1,12 @@
+//https://banter-backend-two.vercel.app/
+import axios from 'axios';
+
+const BASE_URL= import.meta.env.MODE==="development"
+? "http://localhost:5001/api":
+"https://banter-backend-two.vercel.app/"
+
+
+export const axiosInstance= axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+});
