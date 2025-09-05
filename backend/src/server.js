3e62a4f9f5aace9c8a5dcb3004d0eb7184098344
Arya Jhawar <aryajhawar.2004@gmail.com>
2025-09-05 13:30:42 +0530

    added all UI and call feature along with real time messaging

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,11 @@ import {serve} from "inngest/express"
 import { inngest, functions } from '../src/config/inngest.js';
 import chatRoutes from './routes/chat.route.js'
 import * as Sentry from "@sentry/node";
+import cors from "cors";
 
 
 const app = express();
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
 
 app.use(express.json())
 app.use(clerkMiddleware()); //req.auth will be available in the request object
